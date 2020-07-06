dad94fa02dc2ac1574c26a0b6e576cc5884a16c1
Seunghan Yoo <64673825+yooseunghan24@users.noreply.github.com>
2020-07-06 16:52:19 +0900

    DIEU NEO v1.0.0

--- js/artists.js
+++ js/artists.js
@@ -1,15 +1,17 @@
-/* artists 이벤트 */
-const portrait = document.querySelectorAll('.portrait');
-const infoWrap = document.querySelectorAll('.info_wrap');
-const infoPrev = document.querySelectorAll('.prev');
-for(let i=0; i<portrait.length; i++) {
-	portrait[i].addEventListener('click', function(){
-		infoWrap[i].style.display = 'flex';
-		console.log(this, infoWrap[i]);
-	})
-}
-for(let i=0; i<infoPrev.length; i++) {
-	infoPrev[i].addEventListener('click', function(){
-		this.parentNode.parentNode.style.display = 'none';
-	})
+const portrait = document.querySelectorAll(".portrait");
+const infoWrap = document.querySelectorAll(".info_wrap");
+const infoPrev = document.querySelectorAll(".prev");
+for (let i = 0; i < portrait.length; i++) {
+  portrait[i].addEventListener("click", function () {
+    infoWrap[i].style.display = "flex";
+  });
+  portrait[i].addEventListener("mouseover", function () {
+    for (let i of portrait) {
+      i.classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+  infoPrev[i].addEventListener("click", function () {
+    this.parentNode.parentNode.style.display = "none";
+  });
 }
--- js/index.js
+++ js/index.js
@@ -1,32 +1,4 @@
-const dontWork = document.querySelectorAll('[href="#"]');
-for(let i of dontWork) {
-	i.addEventListener('click', function(e){
-		e.preventDefault();
-	})
-}
-const menuBtn = document.querySelector('.menu_btn');
-const nav = document.querySelector('nav');
-const submenu = document.querySelectorAll('nav a');
-let sw = 0;
-menuBtn.addEventListener('click', function(){
-	if(sw === 0 ) {
-		this.classList.remove('no_active');
-		this.classList.add('active');
-		nav.classList.add('active');
-		submenu.forEach(function(item){
-			item.classList.add('active');
-		})
-		sw = 1;
-	} else {
-		this.classList.remove('active');
-		this.classList.add('no_active');
-		sw = 0;
-		nav.classList.remove('active');
-		submenu.forEach(function(item){
-			item.classList.remove('active');
-		})
-	}
-})
+/* 인덱스 이벤트 */
 const html = document.documentElement;
 const subTitle = document.querySelector('.sub_title');
 const pictureWrap = document.querySelectorAll('.picture_wrap');
@@ -38,16 +10,18 @@ function mainImgMove() {
 		pictureWrap[i].style.left = (posX[i] + mL) + 'px';
 	}
 }
-html.addEventListener('wheel', function(e){
-	let delta = e.deltaY;
-	delta > 0 ? html.scrollLeft += delta : html.scrollLeft += delta;
-	if(delta > 0) {
-		if(html.scrollLeft === 1380 || html.scrollLeft === 100) return false;
-		mainImgMove();
-		mL -= 10;
-	} else {
-		if(html.scrollLeft === 0 || html.scrollLeft === 1280) return false;
-		mainImgMove();
-		mL += 10;
-	}
-})
+if(window.innerWidth >= 1024) {
+	html.addEventListener('wheel', function(e){
+		let delta = e.deltaY;
+		delta > 0 ? html.scrollLeft += 50 : html.scrollLeft -= 50;
+		if(delta > 0) {
+			if(html.scrollLeft === 1380 || html.scrollLeft === 100) return false;
+			mainImgMove();
+			mL -= 10;
+		} else {
+			if(html.scrollLeft === 0 || html.scrollLeft === 1280) return false;
+			mainImgMove();
+			mL += 10;
+		}
+	})
+}
